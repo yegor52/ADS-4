733db8861cfa7056b77a89ca8ab3b343e9aa46a0
yegor52 <yegorbeat2005@gmail.com>
2024-03-28 15:28:17 +0300

    ADS-4 Sharov

--- src/alg.cpp
+++ src/alg.cpp
@@ -56,7 +56,7 @@ int cbinsearch(int *arr, int size, int value) {
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len; i++) {
-        count += cbinsearch(&arr[i + 1], len - 1, value - arr[i]);
+        count += cbinsearch(&arr[i + 1], len - i - 1, value - arr[i]);
     }
     return count;
 }
