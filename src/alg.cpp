8ceb8645abe730ded2b757b9ab5de19654718897
yegor52 <yegorbeat2005@gmail.com>
2024-03-28 16:50:48 +0300

    ADS-4 Sharov

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,14 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-    int counter = 0;
+    int count = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                counter++;
+                count++;
             }
         }
     }
-    return counter;
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
